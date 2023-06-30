1031138375f4fb76dc07dd2305e86bf261fee2d0
Nikita Peschanov <keyngr@gmail.com>
2023-06-30 15:34:32 +0400

    dg

--- cats/views.py
+++ cats/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.throttling import ScopedRateThrottle
 
 from .models import Achievement, Cat, User
+from .permissions import OwnerOrReadOnly, ReadOnly
+from .throttling import WorkingHoursRateThrottle    
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
@@ -8,9 +12,20 @@ from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    throttle_scope = 'low_request'
+
+    def get_permissions(self):
+    # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернем обновленный перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions()
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
